055d7cff063ce2dc835c96cf56076a560d45cc7b
TreyBlack01 <blacktrey000@gmail.com>
2024-11-21 14:14:37 -0500

    Removed some redundencies and changed the Burger constructer from __int__ to __init__ so it will take arguments

--- burger.py
+++ burger.py
@@ -1,6 +1,6 @@
 class Burger:
     # Constructor
-    def __int__(self, date, code, country, localPrice, exchangeRate, USD):
+    def __init__(self, date, code, country, localPrice, exchangeRate, USD):
         self.date = date
         self.code = code
         self.country = country
