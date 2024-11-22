24f71e230a90bc9062fa813cd94c9522758e5b94
John Portin <john.portin@proton.me>
2024-11-22 09:17:46 -0800

    Add filler functions for handling user inputs

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import view
 if __name__ == "__main__":
     # Print menu options
     view.print_menu_options()
-    while (user_selection := input("What would you like to do?")):
+    while (user_selection := input("What would you like to do? ")):
         controller.handle_user_input(user_selection)
 
 
