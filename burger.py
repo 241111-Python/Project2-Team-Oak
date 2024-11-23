5d42de38d60019c4ba100d536d266a10ef2bcd25
TreyBlack01 <blacktrey000@gmail.com>
2024-11-23 11:43:12 -0500

    Added a function to display the date as a float

--- burger.py
+++ burger.py
@@ -10,3 +10,6 @@ class Burger:
     
     def __str__(self):
         return f'Date: {self.date} | Currency Code: {self.code} | Country: {self.country:20}| Local Price: {self.localPrice:<8.2f}| Exchange Rate: {self.exchangeRate:<8.2f} | USD: {self.USD:.2f}'
+
+    def date_as_float(self):
+        return (float(self.date[:4]) + (float(self.date[5:7])/12))
