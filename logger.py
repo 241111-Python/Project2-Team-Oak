2e2d88b8f5938c48ea28607513f75085a5f91e65
TreyBlack01 <blacktrey000@gmail.com>
2024-11-26 14:33:05 -0500

    logger update

--- logger.py
+++ logger.py
@@ -1,12 +1,14 @@
 import controller
+import os
 from datetime import datetime
 import csv
 
 def export(report):
     file = f"C:\Revature\Project2-Team-Oak\logged_reports\s_report_{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
-    with open(file,'w') as f:
+    absPath = os.path.abspath(file)
+    with open(absPath,'w') as f:
         f.write(report)
 
 if __name__ == "__main__":
-    r = controller.return_standard_report()
+    r = controller.handle_generate_standard_report()
     export(r)
