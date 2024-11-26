55c7b8e8b10ab2a26d4c68bdd97a774f01c718f3
John Portin <john.portin@proton.me>
2024-11-26 09:14:38 -0800

    Add CLI for generating reports

--- main.py
+++ main.py
@@ -11,18 +11,25 @@ if __name__ == "__main__":
 
     parser.add_argument("--filename", "-f",
                         type=str,
-                        help="Select a different file to be processed instead of the default: bigmac.csv")
+                        help="Select a different file to be processed instead of the default: bigmac.csv"
+                        )
 
 
-    parser.add_argument("--report", "-r",
+    parser.add_argument("--reports", "-r",
                         type=str,
-                        help="Select a report to be produced from \"standard\", \"ppp-<date>\", or \"ppp-<country>\" where date or country must be specified.")
+                        nargs="+",
+                        help="Select one or more reports from \"standard\", \"ppp-<date>\", or \"ppp-<country>\" where date or country must be specified. Separate reports with spaces. Select \"all\" for all reports."
+                        )
     
     args = parser.parse_args()
 
     if args.filename:
         print(f"Processing file: {args.filename}")
         controller.handle_upload_data()
+
+    if args.reports:
+        for report in args.reports:
+            print(report)
 
 
     # Print menu options
