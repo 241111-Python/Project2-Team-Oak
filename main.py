fe659f601ac75f312af3e482961f96c759ca80f2
John Portin <john.portin@proton.me>
2024-11-26 08:35:20 -0800

    Add arg parser. Basic CLI working, but needs additional functionality

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 import controller
 import view
+import argparse
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="Big Mac Data Analyzer",
+        description="This program is intended to allow users to analyze big mac data. User are able to view various subsets of the data by choosing custom filters. They are also able to output reports for later use."
+    )
+
+    parser.add_argument("--filename", "-f",
+                        type=str,
+                        help="Select a different file to be processed instead of the default: bigmac.csv")
+
+
+    parser.add_argument("--report", "-r",
+                        type=str,
+                        help="Select a report to be produced from \"standard\", \"ppp-<date>\", or \"ppp-<country>\" where date or country must be specified.")
+    
+    args = parser.parse_args()
+
+    if args.filename:
+        print(f"Processing file: {args.filename}")
+        controller.handle_upload_data()
+
+
     # Print menu options
     view.print_menu_options()
     while (userSelection := input("What would you like to do? ")):
